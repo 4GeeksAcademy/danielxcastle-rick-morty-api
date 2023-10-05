7927a1da4bcad2286c12e9597a76ba69c3fa03ea
Daniel Cunningham <138261809+danielxcastle@users.noreply.github.com>
2023-10-05 01:02:09 +0000

    update

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from api.utils import APIException
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +12,127 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String(120), nullable=False)
+    species = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Character {self.name}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+            "gender": self.gender,
+        }
+
+
+class Locations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    type = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Location {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id, "name": self.name, "type": self.type}
+
+
+class Episodes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    air_date = db.Column(db.String(120), nullable=False)
+    episode = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Episode {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id, "air_date": self.air_date, "episode": self.episode}
+
+
+
+class FavoriteEpisodes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    episodes_id = db.Column(db.Integer, ForeignKey("episodes.id"))
+    episode = db.relationship("Episodes", backref="favorite_episodes")
+
+    def __repr__(self):
+        return f"<Favorite Episodes {self.name}>"
+
+    def serialize(self):
+        return {"id": self.id, "air_date": self.air_date, "episode": self.episode}
+
+
+class FavoriteLocations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, ForeignKey("locations.id"))
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+    user = db.relationship("User", backref="favorite_locations")
+    location = db.relationship("Locations", backref="marked_as_favorite")
+
+    def __init__(self, user_id, location_id):
+        self.location_id = location_id
+        self.user_id = user_id
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise APIException(f"{error}", 500)
+
+    def __repr__(self):
+        return f"<Favorite Locations {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.serialize(),
+            "location": self.location.serialize()
+        }
+    
+
+class FavoriteCharacters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, ForeignKey("characters.id"))
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+    user = db.relationship("User", backref="favorite_characters")
+    character = db.relationship("Characters", backref="marked_as_favorite")
+
+    def __init__(self, user_id, character_id):
+        self.character_id = character_id
+        self.user_id = user_id
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise APIException(f"{error}", 500)
+
+    def __repr__(self):
+        return f"<Favorite Characters {self.id}>"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.serialize(),
+            "character": self.character.serialize()
+
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,113 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Characters, Locations, Episodes, FavoriteLocations, FavoriteCharacters
 from api.utils import generate_sitemap, APIException
 
-api = Blueprint('api', __name__)
+api = Blueprint("api", __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route("/hello", methods=["POST", "GET"])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
     return jsonify(response_body), 200
+
+
+@api.route("/users", methods=["GET"])
+def handle_users():
+    if request.method != "GET":
+        raise APIException("method not implemented", 405)
+    users = User.query.all()
+    user_dictionaries = []
+    for user in users:
+        user_dictionaries.append(user.serialize())
+    return jsonify(user_dictionaries), 200
+
+
+@api.route("/characters", methods=["GET"])
+def handle_characters():
+    if request.method != "GET":
+        raise APIException("method not implemented", 405)
+    characters = Characters.query.all()
+    character_dictionaries = []
+    for character in characters:
+        character_dictionaries.append(character.serialize())
+    return jsonify(character_dictionaries), 200
+
+
+@api.route("/episodes", methods=["GET"])
+def handle_episodes():
+    if request.method != "GET":
+        raise APIException("method not implemented", 405)
+    episodes = Episodes.query.all()
+    episode_dictionaries = []
+    for episode in episodes:
+        episode_dictionaries.append(episode.serialize())
+    return jsonify(episode_dictionaries), 200
+
+
+@api.route("/locations", methods=["GET"])
+def handle_locations():
+    if request.method != "GET":
+        raise APIException("method not implemented", 405)
+    locations = Locations.query.all()
+    location_dictionaries = []
+    for location in locations:
+        location_dictionaries.append(location.serialize())
+    return jsonify(location_dictionaries), 200
+
+
+@api.route("/favorite/locations/<int:user_id>", methods=["GET", "POST", "DELETE"])
+def handle_favorite_locations(user_id):
+    if request.method == "GET":
+        favorite_locations = FavoriteLocations.query.filter_by(user_id=user_id)
+        favorite_locations_list = []
+        for favorite_location in favorite_locations:
+            favorite_locations_list.append(favorite_location.serialize())
+        return jsonify(favorite_locations_list), 200
+    elif request.method == "POST":
+        body = request.json
+        location_id = body["location_id"]
+        existing_favorite_location = FavoriteLocations.query.filter_by(user_id=user_id, location_id=location_id)
+        if existing_favorite_location is not None:
+            raise APIException("Location already in favorites.", 400)
+        favorite_location = FavoriteLocations(
+            location_id = body["location_id"],
+            user_id = user_id
+        )
+        if not isinstance(
+            favorite_location, FavoriteLocations
+        ):
+          return jsonify("Something went wrong"), 500  
+        return jsonify(favorite_location.serialize()), 201
+    elif request.method == "DELETE":
+        pass
+
+@api.route("/favorite/characters/<int:user_id>", methods=["GET", "POST", "DELETE"])
+def handle_favorite_characters(user_id):
+    if request.method == "GET":
+        favorite_characters = FavoriteCharacters.query.filter_by(user_id=user_id)
+        favorite_characters_list = []
+        for favorite_location in favorite_characters:
+            favorite_characters_list.append(favorite_location.serialize())
+        return jsonify(favorite_characters_list), 200
+    elif request.method == "POST":
+        body = request.json
+        character_id = body["character_id"]
+        existing_favorite_character = FavoriteLocations.query.filter_by(user_id=user_id, character_id=character_id)
+        if existing_favorite_character is not None:
+            raise APIException("Location already in favorites.", 400)
+        favorite_character = FavoriteCharacters(
+            character_id = body["character_id"],
+            user_id = user_id
+        )
+        if not isinstance(
+            favorite_location, FavoriteCharacters
+        ):
+          return jsonify("Something went wrong"), 500  
+        return jsonify(favorite_character.serialize()), 201
+    elif request.method == "DELETE":
+        pass
